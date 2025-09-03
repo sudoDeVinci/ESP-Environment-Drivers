f4fc580c3c6fc59c78d88c91dee08caae8387a27
Tadj Cazaubon <Tadj.d.cazaubon@gmail.com>
2025-09-03 19:45:25 +0200

    include path fixes

--- ESP-Environment-Drivers.ino
+++ ESP-Environment-Drivers.ino
@@ -1,6 +1,6 @@
-#include "MPU6050.hpp"
-#include "SHT31D.hpp"
-#include "BMP3xx.hpp"
+#include "MPU6050/MPU6050.hpp"
+#include "SHT3X/SHT31D.hpp"
+#include "BMP/BMP3xx.hpp"
 
 
 #define I2C_BUS_0_SDA 21
