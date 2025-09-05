f238da126c94cfdb60007702e3819a6c3bbcbdbf
Devinci <hervorarvitr@gmail.com>
2025-09-05 03:28:57 +0200

    I2C_TIMEOUT_MS has an initializer in the header, but MAX_SAMPLES

--- I2CSensor.cpp
+++ I2CSensor.cpp
@@ -1,5 +1,4 @@
 #include "I2CSensor.hpp"
 
-// Define static constexpr members
-constexpr std::chrono::milliseconds I2CSensor::I2C_TIMEOUT_MS;
-constexpr uint16_t I2CSensor::MAX_SAMPLES;
+// Define static const members that don't have constexpr initializers
+const uint16_t I2CSensor::MAX_SAMPLES;
